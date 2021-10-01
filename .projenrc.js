6db0899c9d648f4fbb3b4845c3df6f7dbac26ce2
Ben Force <benforce@gmail.com>
2021-10-01 05:55:14 -0500

    fix: example working

--- .projenrc.js
+++ .projenrc.js
@@ -10,6 +10,7 @@ const project = new AwsCdkTypeScriptApp({
     '@aws-cdk/aws-events',
     '@aws-cdk/aws-events-targets',
     '@aws-cdk/aws-iam',
+    '@aws-cdk/aws-logs',
   ], /* Which AWS CDK modules (those that start with "@aws-cdk/") this app uses. */
   // deps: [],                    /* Runtime dependencies of this module. */
   // description: undefined,      /* The description is just a string that helps people understand the purpose of the package. */
--- src/targetStack.ts
+++ src/targetStack.ts
@@ -1,5 +1,6 @@
 import * as events from '@aws-cdk/aws-events';
-import * as iam from '@aws-cdk/aws-iam';
+import * as eventsTargets from '@aws-cdk/aws-events-targets';
+import * as logs from '@aws-cdk/aws-logs';
 import * as cdk from '@aws-cdk/core';
 
 interface TargetStackProps extends cdk.StackProps {
@@ -20,13 +21,27 @@ export class TargetStack extends cdk.Stack {
     targetBus._enableCrossEnvironment();
     this.bus = targetBus;
 
-    targetBus.grantPutEventsTo(new iam.AccountPrincipal(props.sourceAccountId));
+    new events.CfnEventBusPolicy(this, 'CrossAccountPolicy', {
+      action: 'events:PutEvents',
+      eventBusName: targetBus.eventBusName,
+      principal: props.sourceAccountId,
+      statementId: `AcceptFrom${props.sourceAccountId}`,
+    });
+
+    const eventLogs = new logs.LogGroup(this, 'EventLogs', {
+      logGroupName: 'EventLogs',
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      retention: logs.RetentionDays.ONE_DAY,
+    });
 
     new events.Rule(this, 'TestRule', {
       eventBus: targetBus,
       eventPattern: {
         detailType: ['TestEvent'],
       },
+      targets: [
+        new eventsTargets.CloudWatchLogGroup(eventLogs),
+      ],
     });
   }
 }
